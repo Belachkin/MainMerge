ee91b776783fb26f485e9eb0a1b948e29017a840
Belachkin <belkin.job123@gmail.com>
2024-12-13 20:41:28 +0300

    fix spawn oject + add barriers

--- Assets/Source/Scripts/Systems/Game/LevelSystem.cs
+++ Assets/Source/Scripts/Systems/Game/LevelSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using Kuhpik;
 using Leopotam.EcsLite;
@@ -41,28 +42,97 @@ namespace Source.Scripts.Systems.Game
         
         {
             ClearChildren();
-            
+            StartCoroutine(SpawnWithDelay());
+            // foreach (var obj in config.LevelStartMobs[save.CurrentLevel])
+            // {
+            //     for (int i = 0; i < obj.Value; i++)
+            //     {
+            //         Vector3 rndPosition;
+            //         int attempts = 0;
+            //         do
+            //         {
+            //             rndPosition = new Vector3(
+            //                 Random.Range(minPosition.x, maxPosition.x),
+            //                 Random.Range(minPosition.y, maxPosition.y),
+            //                 Random.Range(minPosition.z, maxPosition.z)
+            //             );
+            //             attempts++;
+            //         } while (Physics.CheckSphere(rndPosition, 0.5f) && attempts < 10); // Проверяем на столкновения
+            //
+            //         var rndRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            //         var newObj = Instantiate(config.MergeObjects[obj.Key], rndPosition, rndRotation, container);
+            //         _levelObjects.Add(newObj);
+            //     }
+            // }
+            //
+            // foreach (var obj in _levelObjects)
+            // {
+            //     var view = obj.GetComponent<BaseView>();
+            //     game.Fabric.InitView(view);
+            // }
+            //
+            // _levelObjects.Clear();
+        }
+        
+        private IEnumerator SpawnWithDelay()
+        {
             foreach (var obj in config.LevelStartMobs[save.CurrentLevel])
             {
-                for (int i = 0; i < obj.Value; i++)
+                int totalToSpawn = obj.Value;
+
+                int maxPerPart = 3;
+                int batchSize = 0;
+                if (totalToSpawn <= maxPerPart)
                 {
-                    var rndPosition = new Vector3(Random.Range(minPosition.x, maxPosition.x), 
-                        Random.Range(minPosition.y, maxPosition.y), 
-                        Random.Range(minPosition.z, maxPosition.z));
-                    var rndRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
-                    
-                    var newObj = Instantiate(config.MergeObjects[obj.Key], rndPosition, rndRotation, container);
-                    
-                    _levelObjects.Add(newObj);
+                    batchSize = totalToSpawn;
+                }
+                else
+                {
+                    batchSize = Mathf.CeilToInt(totalToSpawn / maxPerPart); // Делаем деление на 3
+                }
+                
+                
+            
+                int spawned = 0;
+                while (spawned < totalToSpawn)
+                {
+                    int spawnThisBatch = Mathf.Min(batchSize, totalToSpawn - spawned); // Считаем, сколько осталось спавнить
+                    for (int i = 0; i < spawnThisBatch; i++)
+                    {
+                        Vector3 rndPosition;
+                        int attempts = 0;
+
+                        do
+                        {
+                            rndPosition = new Vector3(
+                                Random.Range(minPosition.x, maxPosition.x),
+                                Random.Range(minPosition.y, maxPosition.y),
+                                Random.Range(minPosition.z, maxPosition.z)
+                            );
+                            attempts++;
+                        } while (Physics.CheckSphere(rndPosition, 0.5f) && attempts < 10);
+
+                        var rndRotation = Quaternion.Euler(
+                            Random.Range(0, 360),
+                            Random.Range(0, 360),
+                            Random.Range(0, 360)
+                        );
+
+                        var newObj = Instantiate(config.MergeObjects[obj.Key], rndPosition, rndRotation, container);
+                        _levelObjects.Add(newObj);
+                    }
+
+                    spawned += spawnThisBatch;
+                    yield return new WaitForSeconds(0.25f); // Ожидание перед следующим "пакетом"
                 }
             }
-            
+
             foreach (var obj in _levelObjects)
             {
                 var view = obj.GetComponent<BaseView>();
                 game.Fabric.InitView(view);
             }
-            
+
             _levelObjects.Clear();
         }
 
